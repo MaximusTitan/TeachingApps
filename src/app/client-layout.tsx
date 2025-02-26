342f895cede447829e139f6ff6bef15b0877f11a
M. Sai Nandan <sainandan205@gmail.com>
2025-02-26 15:55:55 +0530

    Cleaned Code

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -5,8 +5,6 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { AppSidebar } from "@/components/components-app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { Toaster } from "@/components/ui/toaster";
-import path from "path";
 
 export default function ClientLayout({
   children,
@@ -14,9 +12,6 @@ export default function ClientLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  const [hasCognitiveAssessment, setHasCognitiveAssessment] = useState(false);
-  const [userRole, setUserRole] = useState<string | null>(null);
-  const [userEmail, setUserEmail] = useState<string | null>(null);
   const [userStatus, setUserStatus] = useState<string | null>(null);
 
   
@@ -32,26 +27,7 @@ export default function ClientLayout({
   }
   const showSidebar =
     pathname === "/" ||
-    pathname.startsWith("/tools") ||
-    pathname.startsWith("/dashboard") ||
-    pathname.startsWith("/chatbot") ||
-    pathname.startsWith("/protected") ||
-    pathname.startsWith("/schools") ||
-    pathname.startsWith("/livebot") ||
-    pathname.startsWith("/audiobot") ||
-    pathname.startsWith("/learn-ai") ||
-    pathname.startsWith("/payment") ||
-    pathname.startsWith("/canvas") ||
-    pathname.startsWith("/rooms") ||
-    pathname.startsWith("/games") ||
-    pathname.startsWith("/logs") ||
-    pathname.startsWith("/roles") ||
-    pathname.startsWith("/document-vault") ||
-    pathname.startsWith("/connect-database") ||
-    pathname.startsWith("/feedback") ||
-    (pathname.startsWith("/profile") &&
-      userRole === "Student" &&
-      hasCognitiveAssessment);
+    pathname.startsWith("/tools");
   return (
     <SidebarProvider>
       <ThemeProvider
@@ -64,7 +40,6 @@ export default function ClientLayout({
         <main className="flex-1 flex flex-col">
           <div>{children}</div>
         </main>
-        <Toaster />
       </ThemeProvider>
     </SidebarProvider>
   );
--- src/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-export function getCurrentUrlExtension(url: string): string {
-    const urlParts = new URL(url).pathname.split("/").filter(Boolean); // Remove empty parts
-    return urlParts.pop() || ""; // Return the last part or an empty string if none
-}
-  
